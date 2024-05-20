af695120f7304dc0fcfdb606add9e031c534e5c6
suyoulin <168067232+suyoulin@users.noreply.github.com>
2024-05-20 14:53:21 +0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/Linksys MR8300/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Linksys/g' package/base-files/files/bin/config_generate
 
 # Open wifi
 # sed -i 's/36/44/g;s/VHT80/HE20/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
