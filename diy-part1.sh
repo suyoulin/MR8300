febc6e069d09ff86ad79177ab620aa92f4e2f439
suyoulin <168067232+suyoulin@users.noreply.github.com>
2024-05-21 23:32:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git kiddin https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kid https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
