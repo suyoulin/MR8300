193541564ace60325e188c247f16e8caa98bbee3
suyoulin <168067232+suyoulin@users.noreply.github.com>
2024-05-21 21:59:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,6 +24,6 @@ sed -i 's/OpenWrt/Linksys/g' package/base-files/files/bin/config_generate
 # sed -i 's/disabled=1/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-# ./scripts/feeds install -a
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a
