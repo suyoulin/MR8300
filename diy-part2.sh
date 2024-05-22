d4300a8c5aa38e96e91071bb6d37453e7f974fd6
suyoulin <168067232+suyoulin@users.noreply.github.com>
2024-05-22 11:59:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,7 @@ sed -i 's/OpenWrt/Linksys/g' package/base-files/files/bin/config_generate
 # sed -i 's/36/44/g;s/VHT80/HE20/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 # sed -i 's/disabled=1/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/PKG_VERSION:=1.8.11/PKG_VERSION:=1.8.14/g' kid/openwrt-packages/xray-core/Makefile
 
 rm -rf feeds/packages/lang/golang
 # rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
